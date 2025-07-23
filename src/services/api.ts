58684c8456d6e78f047f202632ec8e44c62b563d
Calvin Sass <calvinsass@outlook.com>
2025-07-23 14:09:17 +0200

    corrected the BASE URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -19,7 +19,7 @@ import type {
 // Create API instance
 const API_BASE_URL =
   import.meta.env.VITE_API_URL ||
-  "https://ec2-13-61-104-235.eu-north-1.compute.amazonaws.com/api";
+  "https://ec2-13-61-104-235.eu-north-1.compute.amazonaws.com:5001/api";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
